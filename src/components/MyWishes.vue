2e4c21ef702888dd4265a9529e4650922157954b
Sucepls <mafsharian45@gmail.com>
2024-06-10 22:53:52 +0330

    handle wishes

--- src/components/MyWishes.vue
+++ src/components/MyWishes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card w-full bg-base-100/90 shadow-xl max-h-52">
+  <div class="card w-full bg-base-100/90 shadow-xl ">
     <div class="card-body">
       <h2 class="card-title justify-center">آرزو های من</h2>
       <PerfectScrollbar class="wishes-scroll">
@@ -17,30 +17,74 @@
             <p class="textarea-xxl font-semibold">{{ wish.title }}</p>
           </div>
           <p class="textarea-xs wish-description">
-            {{ wish.description }}
+            {{ wish.content }}
           </p>
         </div>
       </PerfectScrollbar>
+      <div class="card-actions justify-center">
+        <button type="submit" class="w-1/2 p-2 bg-blue-700 rounded-md" @click="openModal">
+          اضافه کردن
+        </button>
+      </div>
     </div>
   </div>
+  <dialog id="my_modal_2" class="modal">
+    <div class="modal-box">
+      <form
+          @submit.prevent="handleSubmit"
+          class="form-control mt-7 gap-3 items-center"
+      >
+        <h3 class="font-bold text-lg text-center">
+          اطلاعات کاربری را وارد کنید
+        </h3>
+
+        <input
+            v-model="wish.title"
+            type="text"
+            placeholder="عنوان آرزو"
+            class="input input-bordered w-full max-w-xs"
+        />
+        <input
+            v-model="wish.content"
+            type="text"
+            placeholder="متن آرزو"
+            class="input input-bordered w-full max-w-xs"
+        />
+
+        <div class="modal-action">
+          <button class="btn btn-success">ثبت</button>
+        </div>
+      </form>
+    </div>
+  </dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { PerfectScrollbar } from "vue3-perfect-scrollbar";
-
-const wishesList = ref<{ title: string; description: string }[]>([]);
-
+import type Wish from "@/models/wish";
+import {useWishStore} from "@/stores/wish";
+import {useToast} from "vue-toast-notification";
+const wish = ref({} as Wish)
+const wishesList = ref<Wish[]>([]);
+const wishStore = useWishStore()
+const $toast = useToast();
+const handleSubmit = () => {
+  wishStore.add(wish.value).then(({data}) => {
+    $toast.success("آرزوی شما ثبت شد", {
+      position: "bottom-left",
+    });
+    document.querySelector("#my_modal_2")!.close();
+  })
+}
+const openModal = () => {
+  document.querySelector("#my_modal_2")!.showModal();
+}
 onMounted(() => {
-  wishesList.value = [
-    { title: "آرزو اول", description: " آرزو اول" },
-    { title: "آرزو دوم", description: " آرزو دوم" },
-    { title: "آرزو سوم", description: " آرزو سوم" },
-    { title: "آرزو چهارم", description: " آرزو چهارم" },
-    { title: "آرزو پنجم", description: " آرزو پنجم" },
-    { title: "آرزو ششم", description: " آرزو ششم" },
-    { title: "آرزو هفتم", description: " آرزو هفتم" },
-  ];
+  wishesList.value = [];
+  wishStore.list().then(({data}) => {
+    wishesList.value = data.data as Wish[]
+  })
 });
 </script>
 
---
+++ src/models/wish.ts
@@ -0,0 +1,5 @@
+export default interface Wish {
+    id?: number;
+    title: string;
+    content: string;
+}
